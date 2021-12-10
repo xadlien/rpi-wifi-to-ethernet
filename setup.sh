92653f3294b615fa31901f4f3eb4ebba95f71013
Daniel Martin <xadlien@gmail.com>
2021-12-10 08:33:57 -0500

    forgot to add dhcp configuration

--- setup.sh
+++ setup.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# install packages
+apt update
+apt install -y isc-dhcp-server
+
+# copy dhcp config
+cp configs/dhcpd.conf /etc/dhcp/
+cp configs/isc-dhcp-server /etc/default/
+
+# start dhcp
+systemctl enable isc-dhcp-server
+systemctl start isc-dhcp-server
+
 # install script
 cp inet_forward.sh /usr/local/bin/inet_forward.sh
 chmod 755 /usr/local/bin/inet_forward.sh
